3729a79708ac4658cec527c0999d4233fd054ca9
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-07-05 11:10:57 -0400

    ADDED get and addtask

---
+++ functions/index.js
@@ -0,0 +1,16 @@
+import {onRequest} from "firebase-functions/v2/https";
+import express from "express";
+import cors from "cors";
+import { getTasks , addTask} from "./src/tasks.js";
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// routes:
+// app.get('/function/:arg1/:arg2/:arg3', getFunction)
+app.get('/tasks/:uid', getTasks);
+app.post('/tasks', addTask);
+
+export const api = onRequest(app);
---
+++ functions/tasks.js
@@ -0,0 +1,28 @@
+import { FieldValue } from "firebase-admin/firestore"; // Firestore specific way to get a timestamp
+import db from "./dbConnect.js";
+
+// Database has a collection called tasks, which has documents inside of it
+const coll = db.collection("tasks");
+
+export async function getTasks(req, res) {
+    const { uid } = req.params;
+    // Get all tasks from a given user:
+    const tasks = await coll.where('uid', '==', uid).get();
+    // maps tasks into an array
+    const taskArary = tasks.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    res.send(taskArary);
+}
+
+export async function addTask(req, res) {
+    const {title, uid} = req.body;
+    if(!title || !uid) {
+        res.status(401).send({ success: false, message: 'Not a valid request'});
+        return;
+    }
+    // Create a document with the following field values in it
+    const newTask = {
+        title, uid, done: false, 
+        createdAt: FieldValue.serverTimestamp()
+    }
+    return await coll.add(newTask);
+}
