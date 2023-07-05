33d5159f85794f4d1d46867f08d84943b69e1642
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-07-05 11:17:41 -0400

    fixed imports

--- functions/dbConnect.js
+++ functions/dbConnect.js
@@ -1,6 +1,6 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import creds from '../creds.js';
+import creds from './creds.js';
 
 // conencts us to our firebase Project
 initializeApp({
--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 import {onRequest} from "firebase-functions/v2/https";
 import express from "express";
 import cors from "cors";
-import { getTasks , addTask} from "./src/tasks.js";
+import { getTasks , addTask} from "./tasks.js";
 
 
 const app = express();
